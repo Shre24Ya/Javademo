ab9366ee12fcd669d6d4f1982dbf5f48d32e13db
unknown <shreyabhagole@gmail.com>
2023-06-24 20:51:58 +0530

    Adding file

---
+++ Sum.java
@@ -0,0 +1,19 @@
+package com.demo.add;
+
+import java.util.*;
+public class Sum {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a");
+		a=sc.nextInt();
+		System.out.println("Enter b");
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("Sum of a+b is: "+c);
+		
+
+	}
+
+}
